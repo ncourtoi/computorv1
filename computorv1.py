f88c6f70a251837806a40b9b7474f05a68576b78
Nicolas Courtois <nicolascourtois3@gmail.com>
2025-11-04 11:59:15 +0100

    sqrt(0) case

--- computorv1.py
+++ computorv1.py
@@ -12,6 +12,8 @@ def my_gcd(a, b):
 def my_sqrt(x, epsilon=1e-10):
     if x < 0:
         raise ValueError("Cannot compute square root of negative number.")
+    if x == 0:
+        return 0.0
     guess = x
     while abs(guess * guess - x) > epsilon:
         guess = (guess + x / guess) / 2
